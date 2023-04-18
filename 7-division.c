e66e3f04075d37afcf2ad4092154e54cdc49b2aa
root <root@DESKTOP-DHALUET>
2023-04-17 23:22:56 -0500

    archivo 7

---
+++ 7-division.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+void division(complex c1, complex c2, complex *c3){
+    double divisor = c2.re * c2.re + c2.im * c2.im;
+    (*c3).re = (c1.re * c2.re + c1.im * c2.im) / divisor;
+    (*c3).im = (c1.im * c2.re - c1.re * c2.im) / divisor;
+
+}
